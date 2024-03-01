707d1d3085fc9ccac42579fbfd5a29256e375eb8
RayanGosling <mr.avdonin@inbox.ru>
2024-03-01 16:22:34 +0300

    Added DataGen

---
+++ src/test/java/ru/netology/data/DataGen.java
@@ -0,0 +1,31 @@
+package ru.netology.data;
+import com.github.javafaker.Faker;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGen {
+    private DataGen() {
+    }
+    public static String generateDate(int shift){
+     return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    public static String generateCity(){
+        var cities = new String[]{"Майкоп", "Горно-Алтайск", "Черкесск", "Пермь", "Владивосток","Москва", "Нижний Новгород", "Казань"};
+        return cities[new Random().nextInt(cities.length)];
+    }
+    public static String generateName(String locale){
+        var faker = new Faker(new Locale(locale));
+        String name = faker.name().fullName();
+        if (name.contains("ё")){
+            name = name.replaceAll("ё", "е");
+        }
+        return name;
+    }
+    public static String generatePhone(String locale){
+        var faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+}
--- src/test/java/ru/netology/test/ReplanTest.java
+++ src/test/java/ru/netology/test/ReplanTest.java
@@ -1,43 +1,29 @@
-package ru.netology;
-
+package ru.netology.test;
 import com.codeborne.selenide.Condition;
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-
+import ru.netology.data.DataGen;
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class ReplanTest {
-    private Faker faker;
 
     @BeforeEach
-    void setUpAll() {
-        faker = new Faker(new Locale("ru"));
-    }
-
-    private String generateDate(int addDays, String pattern) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    void setUp() {
+        open("http://localhost:9999");
     }
 
     @Test
     public void shouldReschedule() {
-        open("http://localhost:9999");
-        String name = faker.name().fullName();
-        String phone = faker.phoneNumber().phoneNumber();
-        String firstDate = generateDate(5, "dd.MM.yyyy");
-        String secondDate = generateDate(10, "dd.MM.yyyy");
-        $("[data-test-id='city'] input").setValue("Казань");
+        String firstDate = DataGen.generateDate(5);
+        String secondDate = DataGen.generateDate(10);
+        $("[data-test-id='city'] input").setValue(DataGen.generateCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(firstDate);
-        $("[data-test-id='name'] input").setValue(name);
-        $("[data-test-id='phone'] input").setValue(phone);
+        $("[data-test-id='name'] input").setValue(DataGen.generateName("ru"));
+        $("[data-test-id='phone'] input").setValue(DataGen.generatePhone("ru"));
         $("[data-test-id='agreement']").click();
         $(".button").click();
         $("[data-test-id='success-notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
