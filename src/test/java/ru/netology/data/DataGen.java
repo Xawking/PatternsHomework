09449b69756bff0ae2ed5e14963be028eea329c7
RayanGosling <mr.avdonin@inbox.ru>
2024-03-01 16:23:20 +0300

    Reformated code

--- src/test/java/ru/netology/data/DataGen.java
+++ src/test/java/ru/netology/data/DataGen.java
@@ -1,5 +1,7 @@
 package ru.netology.data;
+
 import com.github.javafaker.Faker;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -8,22 +10,26 @@ import java.util.Random;
 public class DataGen {
     private DataGen() {
     }
-    public static String generateDate(int shift){
-     return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
-    public static String generateCity(){
-        var cities = new String[]{"Майкоп", "Горно-Алтайск", "Черкесск", "Пермь", "Владивосток","Москва", "Нижний Новгород", "Казань"};
+
+    public static String generateCity() {
+        var cities = new String[]{"Майкоп", "Горно-Алтайск", "Черкесск", "Пермь", "Владивосток", "Москва", "Нижний Новгород", "Казань"};
         return cities[new Random().nextInt(cities.length)];
     }
-    public static String generateName(String locale){
+
+    public static String generateName(String locale) {
         var faker = new Faker(new Locale(locale));
         String name = faker.name().fullName();
-        if (name.contains("ё")){
+        if (name.contains("ё")) {
             name = name.replaceAll("ё", "е");
         }
         return name;
     }
-    public static String generatePhone(String locale){
+
+    public static String generatePhone(String locale) {
         var faker = new Faker(new Locale(locale));
         return faker.phoneNumber().phoneNumber();
     }
--- src/test/java/ru/netology/test/ReplanTest.java
+++ src/test/java/ru/netology/test/ReplanTest.java
@@ -1,10 +1,13 @@
 package ru.netology.test;
+
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataGen;
+
 import java.time.Duration;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
